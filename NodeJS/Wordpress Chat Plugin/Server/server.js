640f5b6825941caebdb67fc6cfa4786a36307ce3
Zachary Earle <zearle@Zacharys-MacBook-Pro.local>
2013-07-10 06:52:11 -0500

    Debugging

--- NodeJS/Wordpress Chat Plugin/Server/server.js
+++ NodeJS/Wordpress Chat Plugin/Server/server.js
@@ -2,10 +2,10 @@ var port = 80;
 var io = require('socket.io').listen(port);
 
 io.sockets.on('connection', function (socket) {
-	socket.on('join room', function (room) {
-	    socket.set('room', room, function() { console.log('room ' + room + ' saved'); } );
-	    socket.join(socket.get('room'));
-	});
+	// socket.on('join room', function (room) {
+// 	    socket.set('room', room, function() { console.log('room ' + room + ' saved'); } );
+// 	    socket.join(socket.get('room'));
+// 	});
 
 	socket.emit('message', { message: 'Welcome to Chat!'});
 	socket.on('send', function (data) {
