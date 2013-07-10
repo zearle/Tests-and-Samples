dc7582853d7928448bfceb1c7a8dde5211811beb
Zachary Earle <zearle@Zacharys-MacBook-Pro.local>
2013-07-10 04:39:15 -0500

    Fixes

--- NodeJS/Wordpress Chat Plugin/Server/server.js
+++ NodeJS/Wordpress Chat Plugin/Server/server.js
@@ -3,12 +3,11 @@ var io = require('socket.io').listen(port);
 
 io.sockets.on('connection', function (socket) {
 	socket.on('join room', function (room) {
+	    socket.set('room', room);
 	    socket.join(room);
-		socket.roomID = room;
-		console.log(room);
-		
 	})
-	socket.emit('message', { message: 'Welcome to Chat!' });
+
+	socket.emit('message', { message: 'Test: ' + socket.roomID });
 	socket.on('send', function (data) {
 		io.sockets.in(socket.roomID).emit('message', data)
 	});
