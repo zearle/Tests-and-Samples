87feb1bcfe81372568a8d42e84f55d08553572d6
Zachary Earle <zearle@Zacharys-MacBook-Pro.local>
2013-07-11 00:07:21 -0500

    Changes

--- NodeJS/Wordpress Chat Plugin/Server/server.js
+++ NodeJS/Wordpress Chat Plugin/Server/server.js
@@ -1,18 +1,15 @@
 var port = 80;
-var io = require('socket.io').listen();
-// io.server.removeListener('request', io.server.listeners('request')[1]);
-io.set('origins','*');
+var io = require('socket.io').listen(port);
 
-io.sockets.on('connection', function (socket) {ire
-	// socket.on('join room', function (room) {
-// 	    socket.set('roomID', room, function() { console.log('room ' + room + ' saved'); } );
-// 	    socket.join(socket.get('roomID'));
-// 	});
-
-	socket.emit('message', { message: 'Welcome to Chat!'});
+io.sockets.on('connection', function (socket) {
+	socket.on('join room', function (room) {
+	    socket.join(room);
+		socket.roomID = room;
+	});
+	socket.emit('message', { message: 'Welcome to Chat!' });
 	socket.on('send', function (data) {
-		// io.sockets.in(socket.get('room')).emit('message', data);
-		socket.emit('message', data);
+		io.sockets.in(socket.roomID).emit('message', data)
+		console.log(socket.roomID);
 	});
 });
 
