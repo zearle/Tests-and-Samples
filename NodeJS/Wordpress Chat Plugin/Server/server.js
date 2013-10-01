087fef1080e9c171ca675f927ad9c6160fa782b2
Zachary Earle <zearle@Zacharys-MacBook-Pro.local>
2013-10-01 15:09:42 -0700

    Added Chat History Logs

--- NodeJS/Wordpress Chat Plugin/Server/server.js
+++ NodeJS/Wordpress Chat Plugin/Server/server.js
@@ -1,6 +1,6 @@
 var port = 8080;
 var io = require('socket.io').listen(port);
-var connection_string = "mongodb://zearle90:monsters1@ds047468.mongolab.com:47468/zearle90"
+var connection_string = "mongodb://airshp:tourgigs1@ds047468.mongolab.com:47468/zearle90"
 var mongojs = require('mongojs');
 var db = mongojs(connection_string, ['tourgigs']);
 var chatlogs = db.collection('tourgigs');
@@ -9,8 +9,9 @@ io.set('log level', 1);
 io.sockets.on('connection', function (socket) {
 	socket.on('join room', function (room) {
 	    socket.join(room);
-		socket.set('roomID', room, function(roomID){
-			console.log(roomID);
+		socket.set('roomID', room, function(roomID){});
+		chatlogs.find({chat_room: room}).forEach(function(err, message) {
+			socket.emit('message', message);
 		});
 	});
 	
@@ -20,15 +21,8 @@ io.sockets.on('connection', function (socket) {
 		socket.get('roomID', function(err, roomID) {
 			var time = new Date();
 			io.sockets.in(roomID).emit('message', data);
-			var document = {room: roomID, username: data.username, message: data.message, timestamp: time};
-			db.chatlogs.insert(document, {safe: true}, function(err, records){console.log(“Record added as “+records[0]._id);});
-			logdata = roomID + ' - ' + data.username + ': ' + data.message + '\n';
-			console.log(logdata);
+			var document = {chat_room: roomID, username: data.username, message: data.message, timestamp: time};
+			chatlogs.insert(document, {safe: true}, function(err, records){console.log('Record added as '+records[0]._id);});
 		});
 	});
 });
-
-collection.insert(document, {safe: true}, function(err, records){
-console.log(“Record added as “+records[0]._id);
-});
-
